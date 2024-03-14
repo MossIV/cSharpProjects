b526b6243169f35230c5a92006d056eb87da2ec1
MossIV <waben.wong@gmail.com>
2024-03-15 12:33:34 +1300

    implemented ViewEmails using methods

--- methodViewEmails/Program.cs
+++ methodViewEmails/Program.cs
@@ -15,10 +15,17 @@ string externalDomain = "hayworth.com";
 
 for (int i = 0; i < corporate.GetLength(0); i++) 
 {
-    // display internal email addresses
+    displayEmail(corporate,i);
 }
 
 for (int i = 0; i < external.GetLength(0); i++) 
 {
-    // display external email addresses
+    displayEmail(external,i,externalDomain);
+}
+
+void displayEmail(string[,] emails, int currentEmail,string emailExtension="contoso.com"){
+    string first = emails[currentEmail,0];
+    string firstTwoLetters = first.Substring(0,2).ToLower();
+    string fullEmail = firstTwoLetters+emails[currentEmail,1].ToLower()+"@"+emailExtension;
+    Console.WriteLine(fullEmail);
 }
