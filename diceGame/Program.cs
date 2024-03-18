5db7997e7f9197ff08037a1fd3e6c899429b8a06
MossIV <waben.wong@gmail.com>
2024-03-18 16:43:07 +1300

    Implemented winOrLose method

--- diceGame/Program.cs
+++ diceGame/Program.cs
@@ -12,14 +12,28 @@ void PlayGame()
 
     while (play) 
     {
-        var target;
-        var roll;
+        var target = -1;
+        var roll = -1;
+
+        roll = random.Next(1,7);
+        target = random.Next(1,7);
 
         Console.WriteLine($"Roll a number greater than {target} to win!");
         Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose());
+        Console.WriteLine(WinOrLose(roll, target));
         Console.WriteLine("\nPlay again? (Y/N)");
 
         play = ShouldPlay();
+
+
+    }
+}
+
+string WinOrLose(int roll, int target){
+    if(roll > target){
+        return "You win!";
+    }
+    else{
+        return "You lose!";
     }
 }
